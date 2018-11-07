427118e4ec8a17374154497e70bf5efa3e7eae4f
Gerard Casas Saez <casassg@gmail.com>
2018-11-07 14:08:24 -0700

    fix admin panel

--- hardware/admin.py
+++ hardware/admin.py
@@ -1,17 +1,12 @@
 from django.contrib import admin
-from django.utils.html import format_html
 
 # Register your models here.
 from hardware import models
 
 
 class HardwareTypeAdmin(admin.ModelAdmin):
-    def image_tag(self, obj):
-        return format_html('<img src="{}" width="200"/>'.format(obj.image.url))
 
-    image_tag.short_description = 'Image'
-
-    list_display = ['name', 'image_tag']
+    list_display = ['name', 'description','total_count']
 
 
 class RequestAdmin(admin.ModelAdmin):
